9c2f350b9434af3b79afcf8d68eb7788786aee8c
osvat0rres <osvaldotorres3108@gmail.com>
2026-09-24 19:42:10 -0700

    finish with

--- frontend/frontend/src/pages/NotFound.jsx
+++ frontend/frontend/src/pages/NotFound.jsx
@@ -1,7 +1,11 @@
 
 function NotFound(){
     return (
-        <div>Not Found</div>
+        <div>
+            <h1>404 Not found </h1>
+            <p>the page you're looking for does not exits!</p>
+        </div>
+    
     );
 }
 
